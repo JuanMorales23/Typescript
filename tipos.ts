bac43dfa9bc66ed2aea5423241f612faac450c12
Juan Morales <juanmr2311@gmail.com>
2024-03-18 00:57:14 -0500

    feat: Se agrega archivo donde se explican las funciones

--- tipos.ts
+++ tipos.ts
@@ -1,4 +1,36 @@
-const number = 1;
+const numberVar: number = 1;
 //La inferencia de datos es dejar que Ts tome el tipo con base al valor
 let texto = 'hola';
 let numero = 20;
+let c = numberVar + numero;  //También será numero infiriendo
+
+//Cuando no sabe inferir un tipo, lo marca como any
+//Se le dice que Ignore el tipado de typescript, no que puede tomar cualquier valor y luego pretender ser ello
+let a; //??
+
+//Cuando no se sabe el tipo
+let unknownVar: unknown;
+
+//Type alias
+//Se hace en Pascal Case
+type Hero = {
+    name: string,
+    age: number
+}
+
+//Objetos 
+//Se le asigna el tipo Hero
+let hero: Hero = {
+    name: 'thor',
+    age: 1500
+}
+//Si le intento poner una propiedad nueva pone problema
+// hero.power = 100;
+
+function createHeroes(name: string, age: number): Hero{
+    return {
+        name, age
+    }
+}
+
+const thor = createHeroes('Thor', 1500);
